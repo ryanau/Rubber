a3f7ffca571389182e826fe5f7451841b24c9951
ryanau <ryan.au@hotmail.com>
2015-10-10 13:48:52 -0700

    add slider with value updating

--- Landing.js
+++ Landing.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react-native');
+var Slider = require('react-native-slider');
 var Parse = require('parse').Parse;
 var ParseReact = require('parse-react');
 
@@ -18,19 +19,39 @@ var styles = StyleSheet.create({
     color: '#656565'
   },
   container: {
-    padding: 30,
-    marginTop: 65,
-    alignItems: 'center'
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'stretch',
+    justifyContent: 'center',
   },
 });
 
 var Landing = React.createClass({
 	// mixins: [ParseReact.Mixin],
+	getInitialState: function () {
+		return {
+			price: 1,
+		}
+	},
+	handleSliderChange: function (value) {
+		this.setState({
+			price: Math.floor(value),
+		})
+	},
 	render: function () {
 		return (
 			<View style={styles.container}>
 				<Text style={styles.description}>
 					Need a rubber? We got you.
+				</Text>
+				<Slider
+					value={this.state.price}
+					onValueChange={this.handleSliderChange}
+					minimumValue={1}
+					maximumValue={40}/>
+				<Text style={styles.description}>
+					Willing to pay ${this.state.price} for safe sex
 				</Text>
 			</View>
 		);
