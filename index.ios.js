17866d4927a06a89c83dd9756929f6aba3131501
Kevin Fangzhou Chen <kevin@airbears2-10-142-61-82.AirBears2.1918.Berkeley.EDU>
2015-10-10 16:11:02 -0700

    added react-parse in package.json, added parse api key etc

--- index.ios.js
+++ index.ios.js
@@ -12,6 +12,15 @@ var {
   View,
 } = React;
 
+var Parse = require('parse/react-native');
+var ParseReact = require('parse-react/react-native');
+
+Parse.initialize(
+  'r9aJqU0PByTPhz3kFPCS7d4ZmZyH07nsbMOnqkUf',
+  'lvA5gbQM5WPB0rfWTtxJJuA8BM7KgZRYKb08zuA3'
+);
+
+
 var Rubber = React.createClass({
   render: function() {
     return (
